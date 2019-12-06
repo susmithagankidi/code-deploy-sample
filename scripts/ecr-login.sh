6d878424cb8ccfdca83c5cf8d1652dde8d560dcd
susmithagankidi <gankidisusmitha@gmail.com>
2019-12-06 13:50:26 -0500

    Update ecr-login.sh

--- scripts/ecr-login.sh
+++ scripts/ecr-login.sh
@@ -8,3 +8,5 @@ docker rm -vf $(docker ps -a -q)
 fi
 docker pull 272842187865.dkr.ecr.us-east-1.amazonaws.com/nginx:latesttest
 docker images
+docker run -d -p 5000:5000 --restart always 272842187865.dkr.ecr.us-east-1.amazonaws.com/nginx:latesttest
+docker container ls
