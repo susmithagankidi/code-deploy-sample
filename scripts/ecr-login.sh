9e8e35c56e73924ee64d2116095652e65f1f149a
susmithagankidi <gankidisusmitha@gmail.com>
2019-12-06 11:42:52 -0500

    Create ecr-login.sh

---
+++ scripts/ecr-login.sh
@@ -0,0 +1,10 @@
+$(aws ecr get-login --region us-east-1 --no-include-email)
+if [  -z "$(docker ps -a -q)"  ]; then
+echo "no containers to remove"
+else
+echo "Stopping and removing Containers"
+docker stop $(docker ps -q)
+docker rm -vf $(docker ps -a -q)
+fi
+docker pull 272842187865.dkr.ecr.us-east-1.amazonaws.com/nginx:latesttest
+docker images
